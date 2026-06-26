07196571cb1f01f0150214f684eba002c23e93f7
Harshght <harshkumargog@gmail.com>
2026-06-27 04:21:08 +0530

    class

---
+++ classes/class.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    class cylinder
+    {
+    private:
+        double b_radius = 1.1;
+        double height = 2;
+    
+    };
+    
+    
+    
+    
+    
+}
